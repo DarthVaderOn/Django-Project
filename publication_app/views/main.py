26f3f233d1fb01a86c7f2dab996d4fc62feb846e
DarthWaderOn <mr.sokol913@yandex.ru>
2022-06-30 13:53:01 +0300

    work on mistakes

--- publication_app/views/main.py
+++ publication_app/views/main.py
@@ -42,29 +42,14 @@ class MainPageView(View):
     def get_tags(request, tag_id):
         """Представление тегов"""
 
-        subscriber = FollowRequest.objects.filter(user=request.user.pk)
-
-        users_follow = []
-
-        for users in subscriber:
-
-            if users.user_follow.pk not in users_follow:
-                users_follow.append(users.user_follow.pk)
-
-        if subscriber:
-
-                posts = Post.objects.filter(tag_id=tag_id).order_by('-id').filter(user_id__in=users_follow).all()
-                tag = Tag.objects.annotate(count=Count("post")).order_by("-count")[:5]
-                image_post = Media.objects.all()
-                tags = Tag.objects.get(pk=tag_id)
-                return render(request, 'tag.html', {
-                    'title': 'Categories',
-                    'posts': posts,
-                    'image_post': image_post,
-                    'tag': tag,
-                    'tags': tags,
-                })
-        else:
-            contex = {'title': 'Categories',
-                      }
-        return render(request, 'main_page.html', contex)
+        posts = Post.objects.filter(tag_id=tag_id).order_by('-id').all()
+        tag = Tag.objects.annotate(count=Count("post")).order_by("-count")[:5]
+        image_post = Media.objects.all()
+        tags = Tag.objects.get(pk=tag_id)
+        return render(request, 'tag.html', {
+            'title': 'Categories',
+            'posts': posts,
+            'image_post': image_post,
+            'tag': tag,
+            'tags': tags,
+        })
